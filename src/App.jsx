7393e192a1984318ca259bd458ada2312c93ca50
maphuthaaggrey-prog <maphuthaaggrey83@gmail.com>
2025-03-20 15:33:25 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,77 @@
-import Hero from './assets/components/Hero'
-import Header from './assets/components/Header'
-import About from './assets/components/About'
-import Skills from './assets/components/Skills'
-import Projects from './assets/components/Projects'
-import Footer from './assets/components/Footer'
+import { useEffect, useState } from 'react';
+import Hero from './assets/components/Hero';
+import Header from './assets/components/Header';
+import About from './assets/components/About';
+import Skills from './assets/components/Skills';
+import Projects from './assets/components/Projects';
+import Footer from './assets/components/Footer';
+import { Helmet } from "react-helmet";
+
 function App() {
+  const [pageTitle, setPageTitle] = useState("Maphutha Aggrey");
 
+  const handleScroll = () => {
+    const sections = document.querySelectorAll("section");
+    let currentSection = "Maphutha Aggrey";
 
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+
+      if (window.scrollY >= sectionTop - sectionHeight / 3) {
+        currentSection = section.getAttribute("id");
+      }
+    });
+
+    
+    switch (currentSection) {
+      case "Hero":
+        setPageTitle("Maphutha Aggrey");
+        break; 
+      case "About":
+        setPageTitle("About");
+        break;
+      case "Projects":
+        setPageTitle("Projects");
+        break;
+      case "Contact":
+        setPageTitle("Contact");
+        break;
+      default:
+        setPageTitle("Aggrey");
+        break;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <>
-          <Header />
-          <section id="Hero">
-              <Hero />
-          </section>
-          <section id="About">
-              <About />
-          </section>
-              <Skills />
-          <section id="Projects">
-              <Projects />
-          </section>
-          <section id="Contact">
-              <Footer />
-          </section>
-
-          
-         
+      <Helmet>
+        <title>{pageTitle}</title>
+      </Helmet>
+      
+      <Header />
+      <section id="Hero">
+        <Hero />
+      </section>
+      <section id="About">
+        <About />
+      </section>
+      <Skills />
+      <section id="Projects">
+        <Projects />
+      </section>
+      <section id="Contact">
+        <Footer />
+      </section>
     </>
-  )
+  );
 }
-export default App
+
+export default App;
--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -1,3 +1,4 @@
+
 const About = () => {
     return ( 
         <section className="hero-about">
