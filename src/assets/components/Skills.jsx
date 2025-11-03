bce942215cf7242053f657e045178fa15d12eeae
maphuthaaggrey-prog <maphuthaaggrey83@gmail.com>
2025-11-04 01:24:17 +0200

    Refactor skills component to display skill percentages and improve layout

--- src/assets/components/Skills.jsx
+++ src/assets/components/Skills.jsx
@@ -21,28 +21,40 @@ const Skills = () => {
       <div className="progress-container">
         <div className="designer-skills">
           <div className="skill">
-            <h3>Figma</h3>
+              <div className="skills-text">
+                <h3>Figma</h3>
+                <h3>90%</h3>
+              </div>
             <div className="progress-bar">
-              <div className="progress" data-width="88%"></div>
+              <div className="progress" data-width="90%"></div>
             </div>
           </div>
 
           <div className="skill">
-            <h3>Adobe XD</h3>
+            <div className="skills-text">
+              <h3>Adobe XD</h3>
+              <h3>85%</h3>
+            </div>
             <div className="progress-bar">
               <div className="progress" data-width="85%"></div>
             </div>
           </div>
 
           <div className="skill">
-            <h3>Adobe Photoshop</h3>
+            <div className="skills-text">
+                <h3>Adobe Photoshop</h3>
+                <h3>35%</h3>
+            </div>
             <div className="progress-bar">
               <div className="progress" data-width="35%"></div>
             </div>
           </div>
 
           <div className="skill">
-            <h3>Git & Github</h3>
+          <div className="skills-text">
+              <h3>Git & Github</h3>
+              <h3>75%</h3>
+          </div>
             <div className="progress-bar">
               <div className="progress" data-width="65%"></div>
             </div>
@@ -51,32 +63,46 @@ const Skills = () => {
 
         <div className="developer-skills">
           <div className="skill">
-            <h3>HTML5 & CSS</h3>
+              <div className="skills-text">
+                <h3>HTML5 & CSS</h3>
+                <h3>95%</h3>
+              </div>
             <div className="progress-bar">
               <div className="progress" data-width="95%"></div>
             </div>
           </div>
 
           <div className="skill">
+          <div className="skills-text">
+
             <h3>JavaScript (ES6+)</h3>
+            <h3>68%</h3>
+            </div>
             <div className="progress-bar">
               <div className="progress" data-width="68%"></div>
             </div>
           </div>
+          
+          <div className="skill">
+          <div className="skills-text">
+            <h3>React JS</h3>
+            <h3>60%</h3>
+          </div>
+            <div className="progress-bar">
+              <div className="progress" data-width="56%"></div>
+            </div>
+          </div>
 
           <div className="skill">
+          <div className="skills-text">
             <h3>PHP & MySQL</h3>
+            <h3>40%</h3>
+          </div>
             <div className="progress-bar">
               <div className="progress" data-width="40%"></div>
             </div>
           </div>
 
-          <div className="skill">
-            <h3>React & Next js</h3>
-            <div className="progress-bar">
-              <div className="progress" data-width="56%"></div>
-            </div>
-          </div>
 
         </div>
       </div>
